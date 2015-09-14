feac87a23f9212ba4ba76d2ca6766962a2fc3fb7
KpaBap <kpabap@gmail.com>
2015-09-13 20:25:19 -0700

    Update hue-flux.py

--- hue-flux.py
+++ hue-flux.py
@@ -1,4 +1,4 @@
-#   Copyright Iavor Todorov
+#   Copyright 2015 Iavor Todorov
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
